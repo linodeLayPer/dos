53ffab206f94058f5e6c2c13518fb4b1f0318c8e
Tokaaaaage <104607502+Tokaaaaage@users.noreply.github.com>
2022-05-05 22:18:07 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ apt install git -y
 apt install golang -y
 ulimit -n 999999
 git clone https://github.com/Leeon123/Stress-tester.git
-cd Stress-test
+cd Stress-tester
 go build stress.go
 wget https://raw.githubusercontent.com/Tokaaaaage/dos/main/dos.sh
 chmod 777 dos.sh

